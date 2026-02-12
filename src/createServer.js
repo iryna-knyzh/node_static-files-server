a6a48cf1d35cd959a988ee2efe41b5e3bbca8d9d
Iryna Knyzh <iryna.knyzh@gmail.com>
2026-02-12 13:36:31 +0200

    task

--- src/createServer.js
+++ src/createServer.js
@@ -1,10 +1,62 @@
 'use strict';
 
+const http = require('node:http');
+const fsp = require('node:fs/promises');
+const path = require('node:path');
+
+const publicDir = path.resolve(__dirname, '../public');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  return http.createServer(async (req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const { pathname } = url;
+
+    // duplicated slashes
+    if (pathname.includes('//')) {
+      res.statusCode = 404;
+
+      return res.end('Double slashes are prohibited!');
+    }
+
+    // only /file routes
+    if (!pathname.startsWith('/file')) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Use /file/pathToFile to load the file');
+    }
+
+    // /file or /file/
+    if (pathname === '/file' || pathname === '/file/') {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Use /file/pathToFile to load the file');
+    }
+
+    // get relative path
+    const relativePath = pathname.replace('/file/', '');
+    const finalPath = path.resolve(publicDir, relativePath);
+
+    // traversal protection
+    if (!finalPath.startsWith(publicDir)) {
+      res.statusCode = 400;
+
+      return res.end('Access denied!');
+    }
+
+    try {
+      const file = await fsp.readFile(finalPath, 'utf-8');
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(file);
+    } catch {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Not Found');
+    }
+  });
 }
 
-module.exports = {
-  createServer,
-};
+module.exports = { createServer };
